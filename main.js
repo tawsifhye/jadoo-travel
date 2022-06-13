135df03e8ba05448dea141bdbd1d81e13237c539
Md Tawsif Ul Hye Chowdhury <tawsif.lucse@gmail.com>
2022-06-13 13:42:01 +0600

    video modal and hero done

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const mobileMenuButton = document.getElementById("mobile-menu-button");
 const mobileMenu = document.getElementById("mobile-menu");
 const mobileMenuButtonClose = document.getElementById("menu-close-button");
+const openVideoModalButton = document.getElementById("play-video");
+const videoModal = document.getElementById("modal");
+const modalCloseButton = document.getElementById("close-modal");
+
+
 
 mobileMenuButton.addEventListener('click', () => {
     mobileMenu.setAttribute("style", "left: 0");
@@ -12,4 +17,13 @@ mobileMenuButtonClose.addEventListener('click', () => {
     mobileMenu.setAttribute("style", "left: -100%");
     mobileMenuButtonClose.setAttribute("style", "display: none");
     mobileMenuButton.setAttribute("style", "display: block");
+});
+
+openVideoModalButton.addEventListener('click', () => {
+    videoModal.setAttribute("style", "display: flex")
+})
+
+
+modalCloseButton.addEventListener('click', () => {
+    videoModal.setAttribute("style", "display: none")
 })
